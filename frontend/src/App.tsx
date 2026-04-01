75f4b657be8fcc6cc706dc503d4b6bda92b29f42
Aleksandr Egorov <aegoro@taltech.ee>
2026-04-01 14:28:02 +0300

    React/Typescript frontend added through vite. Tailwindcss connected.

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+
+function App() {
+  return (
+    <div className='p-8'>
+      <h1 className='text-3xl font-bold mb-4 text-blue-600'>Book API Frontend</h1>
+    </div>
+  );
+}
+
+
+export default App;
