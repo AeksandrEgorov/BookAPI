cf14292d049c4a10aa77c18e20c7de0c57a9e92b
Aleksandr Egorov <aegoro@taltech.ee>
2026-04-08 13:05:48 +0300

    Added books page with filtering. Buttons dont work yet.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,10 @@
 import { Routes, Route } from 'react-router-dom'
 import BooksPage from './pages/BooksPage'
-import BooksDetailPage from './pages/BooksDetailPage'
 
 function App() {
   return (
     <Routes>
-      <Route path= "/books" />
+      <Route path= "/books" element={<BooksPage />} />
       <Route path= "/books/:id" />
     </Routes>
   );
